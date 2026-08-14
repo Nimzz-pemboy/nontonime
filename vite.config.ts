26c5052f4b36e53477bd658b28f0c27ed93ac6a1
Nimzz-pemboy <naimffbisa828@gmail.com>
2026-08-14 19:42:13 +0700

    i love you lejen-kungit add .! >_<

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
-import nitroV2Plugin from "nitro/vite";
+import { nitro as nitroV2Plugin } from "nitro/vite";
 
 export default defineConfig({
   server: {
